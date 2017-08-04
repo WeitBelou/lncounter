5ecb1e6c2792d841417855aae75dded42f2a1fbb
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-08-04 15:00:12 +0300

    Add possibility to count lines in several files (just like `wc -l` do).
    Signed-off-by: Ivan Kosolapov <ivan.kosolapov@phystech.edu>

--- lncounter/main.go
+++ lncounter/main.go
@@ -6,28 +6,45 @@ import (
 	"os"
 )
 
-func main() {
-	nArgs := len(os.Args)
-	if nArgs == 1 {
-		log.Print("Unexpected number of arguments", nArgs-1)
-		os.Exit(1)
-	}
-	filename := os.Args[1]
-
+func processFile(filename string) {
 	file, err := os.Open(filename)
-
 	if err != nil {
 		log.Print(err)
 		os.Exit(1)
 	}
 
 	nLines, err := lncounter.CountLines(file)
+
 	if err != nil {
 		log.Print(err)
 		os.Exit(1)
 	}
-
 	println(nLines, filename)
 
 	file.Close()
+}
+
+func processSeveralFiles(files []string) {
+	nFiles := len(files)
+
+	for i := 0; i < nFiles; i++ {
+		processFile(files[i])
+	}
+}
+
+func main() {
+	nArgs := len(os.Args)
+	if nArgs == 1 {
+		log.Print("Unexpected number of arguments", nArgs-1)
+		os.Exit(1)
+	}
+
+	filenames := os.Args[1:]
+	nFiles := len(filenames)
+
+	if nFiles == 1 {
+		processFile(filenames[0])
+	} else {
+		processSeveralFiles(filenames)
+	}
 }
