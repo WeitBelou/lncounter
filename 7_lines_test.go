98199c3ccf36efe7725bd53e518a630013ca3489
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-08-04 13:54:08 +0300

    Create simple test for lncounter.

---
+++ 7_lines_test.go
@@ -0,0 +1,20 @@
+package lncounter
+
+import (
+    "testing"
+    "os"
+)
+
+func Test7Lines(t *testing.T) {
+    file, _ := os.Open("./7_lines.go")
+
+    nLines, err := CountLines(file)
+
+    if err != nil {
+        t.Error(err)
+    }
+
+    if nLines != 7 {
+        t.Error()
+    }
+}
---
+++ lncounter.go
@@ -0,0 +1,11 @@
+package lncounter
+
+import "io"
+
+func CountLines(r io.Reader) (int, error) {
+    return 1, nil
+}
+
+func main() {
+
+}
